384ff78cdc784330e44a4337ba67232ceeeadc56
upqode-bohdan <bogdan@upqode.com>
2018-07-30 23:35:07 +0300

    lesson 3

---
+++ dist/const.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var MATH = {
+    PI: 3.14159
+};
+
+MATH.PI = 45;
+console.log(MATH.PI);
---
+++ src/const.js
@@ -0,0 +1,6 @@
+const MATH = {
+    PI: 3.14159,
+}
+
+MATH.PI = 45;
+console.log(MATH.PI);

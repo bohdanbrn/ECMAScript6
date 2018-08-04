93aefad6a7d228f85a761c9d873befdef3053b6c
upqode-bohdan <bogdan@upqode.com>
2018-08-04 18:40:57 +0300

    lesson 7 (for...of)

---
+++ dist/for_of.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var browsers = ['Chrome', 'FireFos', 'Edge', 'Safari', 'Opera'];
+
+for (var index in browsers) {
+    console.log(browsers[index]);
+}
+
+console.log("----------");
+
+var _iteratorNormalCompletion = true;
+var _didIteratorError = false;
+var _iteratorError = undefined;
+
+try {
+    for (var _iterator = browsers[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        var browser = _step.value;
+
+        console.log(browser);
+    }
+} catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+} finally {
+    try {
+        if (!_iteratorNormalCompletion && _iterator.return) {
+            _iterator.return();
+        }
+    } finally {
+        if (_didIteratorError) {
+            throw _iteratorError;
+        }
+    }
+}
---
+++ src/for_of.js
@@ -0,0 +1,11 @@
+let browsers = ['Chrome', 'FireFos', 'Edge', 'Safari', 'Opera'];
+
+for ( let index in browsers) {
+    console.log(browsers[index]);
+}
+
+console.log("----------");
+
+for ( let browser of browsers) {
+    console.log(browser);
+}
